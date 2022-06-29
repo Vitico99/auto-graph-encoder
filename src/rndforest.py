1b12a2afe17b1da8073539a9855fdf23eb96b566
Karla Olivera Hernández <karlaolivera99@gmail.com>
2022-06-29 12:03:46 -0400

    Optimize params of SVC, RandomForest and NaiveBayes models

--- src/rndforest.py
+++ src/rndforest.py
@@ -3,7 +3,7 @@ from sklearn.ensemble import RandomForestClassifier
 
 class RandomForestModel(Model):
     def __init__(self):
-        self.rfst = RandomForestClassifier()
+        self.rfst = RandomForestClassifier(min_samples_leaf=5, min_samples_split=8, n_estimators=1000)
 
     def train(self, X, Y):
         self.rfst.fit(X, Y)
